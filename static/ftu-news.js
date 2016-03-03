1f54cd9ebdd22b44a6a0dcdc5a8b0aaaeefafe11
minhduc150496 <minhduc150496@gmail.com>
2016-03-03 16:15:42 +0700

    Details Page with Responsive

---
+++ static/ftu-news.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var curSlide, nSlide, widSlide;
+window.onload = function() {
+	curSlide = 0;
+	nSlide = 5;
+	widSlide = document.getElementById("slides-container").getElementsByTagName("div").item(0).clientWidth+1;
+}
+function prevSlide() {
+	if (curSlide==0) return;
+	var s = document.getElementById("slides-container");
+	curSlide--;
+	s.style.transform = 'translateX(' + curSlide*(-widSlide) + 'px)';
+}
+function nextSlide() {
+	if (curSlide+1==nSlide) return;
+	var s = document.getElementById("slides-container");
+	curSlide++;
+	s.style.transform = 'translateX(' + (curSlide*(-widSlide)-1) + 'px)';
+}
